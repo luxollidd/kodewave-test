5597abb4167fdda89c8f6144e1ff7bd56f435e1b
lux <luxollidd@gmail.com>
2022-06-10 10:53:31 +0000

    change home route to redirect to login

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Http\Request;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 Route::middleware('auth')->group(function() {
